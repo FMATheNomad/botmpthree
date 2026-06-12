5e8bfaf488a488e7c31fefb15bac98961bd96bc1
FMATheNomad <farizmuhammadaditya682@gmail.com>
2026-06-12 20:21:13 +0700

    update bot token

--- bot_ytmp3.py
+++ bot_ytmp3.py
@@ -5,7 +5,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQu
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-TOKEN = os.getenv("YTMP3_TOKEN", "8621575752:AAEeaPpFsBqoKugNODOynd4jESl9sJOSy0M")
+TOKEN = os.getenv("YTMP3_TOKEN", "8621575752:AAGNPpfJPt47e-u3E4ciVvTiqearqvQ4HzU")
 MAX_SIZE_MB = 50
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
